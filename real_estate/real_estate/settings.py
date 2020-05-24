93f37b40abe0a77378ffd06ae35f68ea735ffff8
Collins Anele <45266007+collinsanele@users.noreply.github.com>
2020-05-24 09:28:37 +0100

    Update settings.py

--- real_estate/real_estate/settings.py
+++ real_estate/real_estate/settings.py
@@ -75,12 +75,13 @@ WSGI_APPLICATION = 'real_estate.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+#DATABASES = {
+    #'default': {
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    #}
+#}
+
 
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=600)
