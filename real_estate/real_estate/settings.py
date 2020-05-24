013a662909e8d7106397a288928961a14a0da72f
Collins Anele <45266007+collinsanele@users.noreply.github.com>
2020-05-24 17:24:52 +0100

    Update settings.py

--- real_estate/real_estate/settings.py
+++ real_estate/real_estate/settings.py
@@ -83,11 +83,12 @@ WSGI_APPLICATION = 'real_estate.wsgi.application'
     #}
 #}
 
-DATABASES = {}
+DATABASES = {'default':''}
+
 
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env)
+DATABASES['default'] = db_from_env
 
 
 
